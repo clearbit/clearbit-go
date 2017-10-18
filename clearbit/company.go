6fc0baeee109a2f2e84d9cd54a0c9f349f3b6868
Philip I. Thomas <mail@philipithomas.com>
2017-10-17 17:33:06 -0700

    update company struct with JSONtStruct and latest API response

--- clearbit/company.go
+++ clearbit/company.go
@@ -17,88 +17,82 @@ type Company struct {
 	LegalName     string   `json:"legalName"`
 	Domain        string   `json:"domain"`
 	DomainAliases []string `json:"domainAliases"`
-	URL           string   `json:"url"`
-
-	Site struct {
-		URL             string   `json:"url"`
-		Title           string   `json:"title"`
-		H1              string   `json:"h1"`
-		MetaDescription string   `json:"metaDescription"`
-		MetaAuthor      string   `json:"metaAuthor"`
-		PhoneNumbers    []string `json:"phoneNumbers"`
-		EmailAddresses  []string `json:"emailAddresses"`
+	Site          struct {
+		PhoneNumbers   []string `json:"phoneNumbers"`
+		EmailAddresses []string `json:"emailAddresses"`
 	} `json:"site"`
-
 	Category struct {
 		Sector        string `json:"sector"`
 		IndustryGroup string `json:"industryGroup"`
 		Industry      string `json:"industry"`
 		SubIndustry   string `json:"subIndustry"`
+		SicCode       string `json:"sicCode"`
+		NaicsCode     string `json:"naicsCode"`
 	} `json:"category"`
-
-	Tags        []string `json:"tags"`
-	Description string   `json:"description"`
-	FoundedYear int      `json:"foundedYear"`
-	Location    string   `json:"location"`
-
-	Geo struct {
-		StreetNumber string  `json:"streetNumber"`
-		StreetName   string  `json:"streetName"`
-		SubPremise   string  `json:"subPremise"`
-		City         string  `json:"city"`
-		State        string  `json:"state"`
-		StateCode    string  `json:"stateCode"`
-		Country      string  `json:"country"`
-		CountryCode  string  `json:"countryCode"`
-		Lat          float32 `json:"lat"`
-		Lng          float32 `json:"lng"`
+	Tags        []string    `json:"tags"`
+	Description string      `json:"description"`
+	FoundedYear int `json:"foundedYear"`
+	Location    string      `json:"location"`
+	TimeZone    string      `json:"timeZone"`
+	UtcOffset   int         `json:"utcOffset"`
+	Geo         struct {
+		StreetNumber string      `json:"streetNumber"`
+		StreetName   string      `json:"streetName"`
+		SubPremise   string `json:"subPremise"`
+		City         string      `json:"city"`
+		PostalCode   string      `json:"postalCode"`
+		State        string      `json:"state"`
+		StateCode    string      `json:"stateCode"`
+		Country      string      `json:"country"`
+		CountryCode  string      `json:"countryCode"`
+		Lat          float64     `json:"lat"`
+		Lng          float64     `json:"lng"`
 	} `json:"geo"`
-	Logo string `json:"logo"`
-
+	Logo     string `json:"logo"`
 	Facebook struct {
 		Handle string `json:"handle"`
+		Likes  int    `json:"likes"`
 	} `json:"facebook"`
-
+	LinkedIn struct {
+		Handle string `json:"handle"`
+	} `json:"linkedin"`
 	Twitter struct {
 		Handle    string `json:"handle"`
 		ID        string `json:"id"`
 		Bio       string `json:"bio"`
 		Followers int    `json:"followers"`
 		Following int    `json:"following"`
-		Statuses  int    `json:"statuses"`
-		Favorites int    `json:"favorites"`
 		Location  string `json:"location"`
 		Site      string `json:"site"`
 		Avatar    string `json:"avatar"`
 	} `json:"twitter"`
-
-	LinkedIn struct {
-		Handle string `json:"handle"`
-	} `json:"linkedin"`
-
 	Crunchbase struct {
 		Handle string `json:"handle"`
 	} `json:"crunchbase"`
-
-	EmailProvider bool   `json:"emailProvider"`
-	Type          string `json:"type"`
+	EmailProvider bool        `json:"emailProvider"`
+	Type          string      `json:"type"`
 	Ticker        string `json:"ticker"`
-	Phone         string `json:"phone"`
-
+	Identifiers   struct {
+		UsEIN string `json:"usEIN"`
+	} `json:"identifiers"`
+	Phone   string `json:"phone"`
 	Metrics struct {
-		AlexaUSRank     int    `json:"alexaUSRank"`
-		AlexaGlobalRank int    `json:"alexaGlobalRank"`
-		GoogleRank      int    `json:"googleRank"`
-		Employees       int    `json:"employees"`
-		EmployeesRange  string `json:"employeesRange"`
-		MarketCap       int    `json:"marketCap"`
-		Raised          int    `json:"raised"`
-		AnnualRevenue   int    `json:"annualRevenue"`
+		AlexaUsRank            int         `json:"alexaUsRank"`
+		AlexaGlobalRank        int         `json:"alexaGlobalRank"`
+		Employees              int         `json:"employees"`
+		EmployeesRange         string      `json:"employeesRange"`
+		MarketCap              string `json:"marketCap"`
+		Raised                 int         `json:"raised"`
+		AnnualRevenue          interface{} `json:"annualRevenue"`
+		EstimatedAnnualRevenue int      `json:"estimatedAnnualRevenue"`
+		FiscalYearEnd          interface{} `json:"fiscalYearEnd"`
 	} `json:"metrics"`
-
-	IndexedAt string   `json:"indexedAt"`
-	Tech      []string `json:"tech"`
-}
+	IndexedAt time.Time `json:"indexedAt"`
+	Tech      []string  `json:"tech"`
+	Parent    struct {
+		Domain string `json:"domain"`
+	} `json:"parent"`
+} `json:"company"`
 
 // CompanyFindParams wraps the parameters needed to interact with the Company
 // API through the Find method
